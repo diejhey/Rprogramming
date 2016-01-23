c0b2ee03646bccb222f03b8cdbdd4de3f0c97955
Diejehy Martinez <djaymartinez1980@gmail.com>
2016-01-24 01:19:50 +0800

    First Commit

---
+++ cachematrix.R
@@ -0,0 +1,74 @@
+
+
+makeCacheMatrix <- function(x = matrix()) 
+{
+        # stores the cached value
+        # initialize to NULL
+        cache <- NULL
+
+        # create the matrix in the working environment
+        set <- function(w) 
+	{
+                x <<- w
+                cache <<- NULL
+        }
+
+        # get the value of the matrix
+        get <- function() x
+        # invert the matrix and store in cache
+        setMatrix <- function(inverse) cache <<- inverse
+        # get the inverted matrix from cache
+        getInverse <- function() cache
+
+        # return the created functions to the working environment
+        list(set = set, get = get,
+             setMatrix = setMatrix,
+             getInverse = getInverse)
+}
+
+
+
+#cache Solve Function below
+
+cacheSolve <- function(x, ...) {
+        ## get the inverse of the matrix stored in cache
+        cache <- x$getInverse()
+
+        # return inverted matrix from cache if it exists
+        # else create the matrix in working environment
+        if (!is.null(cache)) {
+                message("getting cached data")
+
+                # display matrix in console
+                return(cache)
+        }
+
+        # create matrix since it does not exist
+        matrix <- x$get()
+
+        # make sure matrix is square and invertible
+        # if not, handle exception cleanly
+        tryCatch( {
+                # set and return inverse of matrix
+                cache <- solve(matrix, ...)
+        },
+        error = function(e) {
+                message("Error:")
+                message(e)
+
+                return(NA)
+        },
+        warning = function(e) {
+                message("Warning:")
+                message(e)
+
+                return(NA)
+        },
+        finally = {
+                # set inverted matrix in cache
+                x$setMatrix(cache)
+        } )
+
+        # returns the matrix
+        return (cache)
+}
